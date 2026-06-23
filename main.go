754c739ceb5365abed6117240ae71d6baf917070
962554 <254557023+962554@users.noreply.github.com>
2026-06-23 19:25:46 +0100

    Add new files (initial commit)
    * .gitignore: new file.
    * messages.txt: new file.
    * go.mod: new file.
    * main.go: new file.

---
+++ main.go
@@ -0,0 +1,11 @@
+// -*- mode:go;mode:go-playground -*-
+// Copyright © 2026 P, Rich
+// License: MIT, see LICENSE for details
+
+// This package is an HTTP/1.1 server built from scratch.
+
+package main
+
+func main() {
+	println("I hope I get the job!")
+}
