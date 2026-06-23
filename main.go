97d5da7cd9ac7b6e1f3ad82fbf9c9034c0dfba9e
962554 <254557023+962554@users.noreply.github.com>
2026-06-23 20:27:46 +0100

    Read / write messages.txt in 8-byte chunks
    * main.go: read messages.txt 8 bytes at a time and print data in
    	8-byte chunks

--- main.go
+++ main.go
@@ -3,9 +3,42 @@
 // License: MIT, see LICENSE for details
 
 // This package is an HTTP/1.1 server built from scratch.
-
 package main
 
+import (
+	"errors"
+	"io"
+	"log"
+	"os"
+)
+
+const (
+	msgFile = "messages.txt"
+	bufSize = 8
+)
+
 func main() {
-	println("I hope I get the job!")
+	log.SetFlags(0)
+
+	file, err := os.Open(msgFile)
+	if err != nil {
+		log.Fatalf("error opening file: %s for reading: %s", msgFile, err)
+	}
+	defer file.Close()
+
+	buf := make([]byte, bufSize)
+	for {
+		n, err := file.Read(buf)
+		if err != nil {
+			if n == 0 && errors.Is(err, io.EOF) {
+				break
+			}
+
+			log.Printf("error reading file: %s: %s\n", msgFile, err)
+
+			break
+		}
+
+		log.Printf("read: %s\n", string(buf[:n]))
+	}
 }
